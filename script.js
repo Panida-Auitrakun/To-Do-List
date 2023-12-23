2145b83bc4020d8e2fb68c12c558e2609e765852
Panida Auitrakun <panidaauitrakun1234@gmail.com>
2023-12-23 21:12:19 +0700

    first commit

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const listContainer = document.getElementById("list-container");
 
 function addTask() {
     if(inputBox.value === '') {
+        /* pop-up แสดงเมื่อมีการกด Add แต่ไม่มีการพิมพ์รายการ */
         alert("You must write something!");
     }
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+},  false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
